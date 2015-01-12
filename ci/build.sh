cc4657c8f36d652a0b56964237521ceaf238045b
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-12 16:37:40 +0000

    Added bower and angular as dependency

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ git checkout master
 git config user.name $GIT_NAME
 git config user.email $GIT_EMAIL
 git config credential.helper "store --file=.git/credentials"
-git config remote.origin.url https://github.com/vilmosioo/Sky-Watch.git
+git config remote.origin.url https://github.com/vilmosioo/magma.git
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
 
 echo 'Patching version...'
