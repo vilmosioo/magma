8393f4df5d73da0c91223089b56861e17b6364ba
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-12 14:46:31 +0000

    Set up git identity

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,7 +1,12 @@
 
-echo 'Pre-deploy clean up'
-
+echo 'Switching to master and setting identity for git'
 git checkout master
+git config user.name $GIT_NAME
+git config user.email $GIT_EMAIL
+git config credential.helper "store --file=.git/credentials"
+git config remote.origin.url https://github.com/vilmosioo/Sky-Watch.git
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+
 git rm -r app
 git rm -r grunt
 git rm -r ci
