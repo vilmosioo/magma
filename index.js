eb7764c59e8d4a85e3cdfe679d2c538badb8f5cc
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-14 12:52:11 +0000

    Dist bug fix

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var express = require('express'),
     logger = require('morgan'),
     path = require('path'),
 		exphbs  = require('express-handlebars'),
-    pck = require('./package.json');
+    pck = require('./package.json'),
+		dir = process.env.NODE_ENV === 'development' ? pck.config.app : pck.config.dist;
 
 var app = express(),
 	hbs = exphbs.create({
 		defaultLayout: 'index',
-		layoutsDir: path.join(__dirname, pck.config.app),
-		partialsDir: path.join(__dirname, pck.config.app + '/views')
+		layoutsDir: path.join(__dirname, dir),
+		partialsDir: path.join(__dirname, dir + '/views')
 	});
 
 app.set('views', path.join(__dirname, pck.config.app + '/views'));
@@ -27,9 +28,9 @@ if(process.env.NODE_ENV === 'development'){
     app.use(express.static(path.join(__dirname, pck.config.app)));
 		app.use(express.static(path.join(__dirname, pck.config.tmp)));
 		app.use(express.static(__dirname)); // this is only required for sourcemaps
-    app.get('/:path?', function(req, res){
+		app.get('/:path?', function(req, res){
 			res.render(req.params.path || 'home');
-    });
+		});
 } else {
     // express will not actually serve any static files, this is just a fallback, nginx will take care of this
     app.use(express.static(path.join(__dirname, pck.config.dist)));
