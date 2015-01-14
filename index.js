01fe92ced1ae2db4b4347ee340f3287fd4973ab7
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-14 12:08:09 +0000

    Using handlebars
    And added favicon

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     logger = require('morgan'),
     path = require('path'),
+		exphbs  = require('express-handlebars'),
     pck = require('./package.json');
 
-var app = express();
+var app = express(),
+	hbs = exphbs.create({
+		defaultLayout: 'index',
+		layoutsDir: path.join(__dirname, pck.config.app),
+		partialsDir: path.join(__dirname, pck.config.app + '/views')
+	});
 
+app.set('views', path.join(__dirname, pck.config.app + '/views'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.enable('view cache');
 app.use(logger('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,15 +27,15 @@ if(process.env.NODE_ENV === 'development'){
     app.use(express.static(path.join(__dirname, pck.config.app)));
 		app.use(express.static(path.join(__dirname, pck.config.tmp)));
 		app.use(express.static(__dirname)); // this is only required for sourcemaps
-    app.use(function(req, res){
-        res.sendFile(path.join(__dirname, pck.config.app) + '/index.html');
+    app.get('/:path?', function(req, res){
+			res.render(req.params.path || 'home');
     });
 } else {
     // express will not actually serve any static files, this is just a fallback, nginx will take care of this
     app.use(express.static(path.join(__dirname, pck.config.dist)));
-    app.use(function(req, res){
-        res.sendFile(path.join(__dirname, pck.config.dist) + '/index.html');
-    });
+		app.get('/:path?', function(req, res){
+			res.render(req.params.path || 'home');
+		});
 }
 
 var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || pck.config.port, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function(){
