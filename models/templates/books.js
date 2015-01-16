94e24dfe6da6ff0b9a75065b349a1d1a889f3b06
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-16 17:44:56 +0000

    Not using mock service

--- models/templates/books.js
+++ models/templates/books.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Pr = require('bluebird'),
-	books = require('../../services/books_mock'),
+	books = require('../../services/books'),
 	querystring = require('querystring');
 
 // @param type [search, list]
