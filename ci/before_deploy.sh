8ff819617aa97310e3dcaf3a8c5943a628b8d8df
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-16 12:27:09 +0000

    Moving file to dist

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+mv -i app/scripts/controllers/Search_controller.js dist
+git rm -r app
 git rm -r grunt
 git rm -r ci
 git rm .gitignore
 git rm Gruntfile.js
 git add dist
-git add app
 git commit -m "Saving artefacts"
 
 echo "Done"
