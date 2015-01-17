27a7e870a01e68ca16975e84e347a6d8ac669559
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-17 17:48:05 +0000

    Using google official nodejs library
    Removed request-promise

--- services/books.js
+++ services/books.js
@@ -1,22 +1,15 @@
 'use strict';
 
-var request = require('request-promise'),
-	util = require('util'),
+var util = require('util'),
 	Pr = require('bluebird'),
-	SEARCH = 'https://www.googleapis.com/books/v1/volumes?q=%s&key=' + process.env.GOOGLE_KEY;
+	books = require('googleapis').books('v1');
 
 module.exports = {
 	search: function(q){
 		if(!!q){
-			console.log('Request => ' + util.format(SEARCH, q));
-			return request(util.format(SEARCH, q))
+			return Pr.promisify(books.volumes.list)({q: q, key: process.env.GOOGLE_KEY})
 				.then(function(response){
-					return JSON.parse(response);
-				}, function(err){
-					console.log(err);
-					return {
-						items: []
-					};
+					return response[0];
 				})
 				.then(function(response){
 					return response.items.map(function(item){
