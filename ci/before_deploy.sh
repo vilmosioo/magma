4caeebc54f474fd27008f1c6f909e9feb494541f
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-18 17:30:20 +0000

    Removing build file (not required)

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "export GOOGLE_KEY=$GOOGLE_KEY" > .openshift/action_hooks/build && chmod +x .openshift/action_hooks/build
+#echo "export GOOGLE_KEY=$GOOGLE_KEY" > .openshift/action_hooks/build && chmod +x .openshift/action_hooks/build
 git rm -r app
 git rm -r grunt
 git rm -r ci
 git rm .gitignore
 git rm Gruntfile.js
 git add dist
-git add .openshift
+#git add .openshift
 git commit -m "Saving artefacts"
 
 echo "Done"
