2ae0d4f43389852133d2f0f87cb39ae0e34b0e2d
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-19 10:53:20 +0000

    Using goodreads api

--- services/books.js
+++ services/books.js
@@ -1,23 +1,38 @@
 'use strict';
 
-var util = require('util'),
+var request = require('request-promise'),
+	util = require('util'),
 	Pr = require('bluebird'),
-	books = require('googleapis').books('v1');
+	xml2js = require('xml2js'),
+	parser = Pr.promisify((new xml2js.Parser()).parseString),
+	SEARCH = 'https://www.goodreads.com/search/index.xml?q=%s&key=' + process.env.GOODREADS_KEY;
 
 module.exports = {
 	search: function(q){
 		if(!!q){
-			return Pr.promisify(books.volumes.list)({q: q, maxResults: 40, key: process.env.GOOGLE_KEY})
+			console.log('Request => ' + util.format(SEARCH, q));
+			return request(util.format(SEARCH, q))
 				.then(function(response){
-					return response[0];
+					return parser(response);
+				}, function(err){
+					console.log(err);
+					return {
+						items: []
+					};
 				})
 				.then(function(response){
-					return response.items.map(function(item){
+					return response.GoodreadsResponse.search[0].results[0].work;
+				})
+				.then(function(works){
+					return works.map(function(work){
+						return work.best_book[0];
+					});
+				})
+				.then(function(books){
+					return books.map(function(item){
 						return {
-							title: item.volumeInfo.title,
-							subtitle: item.volumeInfo.subtitle,
-							image: item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : '',
-							snippet: item.searchInfo ? item.searchInfo.textSnippet : ''
+							title: item.title[0],
+							image: item.image_url[0].replace(/(\d+)[m,s]\//, '$1l/')
 						}
 					})
 				});
