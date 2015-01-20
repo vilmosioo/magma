6281aeb52770cb2e9d4825a603b08435a2b97e5e
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-20 14:18:20 +0000

    Formatting search title

--- models/search.js
+++ models/search.js
@@ -3,13 +3,17 @@
 var route = require('./ROUTES.json')['/search/'],
 	books = require('./templates/books'),
 	extend = require('extend'),
+	util = require('util'),
 	Pr = require('bluebird');
 
 module.exports = function(args){
 
 	var data = {
-		title: route.title,
-		books: []
+		title: util.format(route.title, args.query.q),
+		books: [],
+		app: {
+			title: util.format(route.title, args.query.q)
+		}
 	}, _defaults = {
 		offset: 0,
 		limit: 100
