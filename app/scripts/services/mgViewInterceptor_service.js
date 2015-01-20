346016efaeea7c3e4f3653ecfec3f3de7eefd95d
Vilmos Ioo <cool.villi@gmail.com>
2015-01-20 23:18:05 +0000

    Added progress bar

--- app/scripts/services/mgViewInterceptor_service.js
+++ app/scripts/services/mgViewInterceptor_service.js
@@ -1,13 +1,33 @@
 'use strict';
 
 angular.module('Magma')
-	.factory('mgViewInterceptor', function(ROUTES, $injector){
+	.factory('mgViewInterceptor', function(ROUTES, $injector, $rootScope, $timeout){
 		var templates = Object.keys(ROUTES).map(function(key){
 			return ROUTES[key].templateUrl;
 		});
 
+		$rootScope.http = {
+			progress: 0,
+			count: 0,
+			loading: false
+		};
+
+		var timeout;
+		$rootScope.$watch('http.loading', function(value){
+			if(value){
+				timeout = $timeout(function(){
+					$rootScope.http.progress = 100;
+				}, 100);
+			} else {
+				$timeout.cancel(timeout);
+				$rootScope.http.progress = 0;
+			}
+		});
+
 		return {
-			'request': function(config) {
+			request: function(config) {
+				$rootScope.http.count++;
+				$rootScope.http.loading = true;
 				// routeParams is not set yet, we must get the current route instead
 				var $route = $injector.get('$route');
 				if(templates.indexOf(config.url) !== -1){
@@ -16,6 +36,13 @@ angular.module('Magma')
 					}).join('&');
 				}
 				return config;
+			},
+			response: function(response){
+				$rootScope.http.count--;
+				if(!$rootScope.http.count){
+					$rootScope.http.loading = false;
+				}
+				return response;
 			}
 		};
 	});
