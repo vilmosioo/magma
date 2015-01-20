5bfb3efccc03d88ac12d769e09b4e1d507253879
Vilmos Ioo <cool.villi@gmail.com>
2015-01-20 21:50:48 +0000

    Support for animations in mgview

--- app/scripts/directives/mgView_directive.js
+++ app/scripts/directives/mgView_directive.js
@@ -1,21 +1,22 @@
 'use strict';
 
 angular.module('Magma')
-	.directive('mgView', function($rootScope, $compile){
+	.directive('mgView', function($animate, $rootScope, $compile){
 		return {
 			restrict: 'A',
 			replace: false,
-			scope: true,
 			priority: 1000, // We set our custom directive's priority to a high number to ensure that it will be compiled first and with terminal: true, the other directives will be skipped after this directive is compiled.
 			link: function(scope, el){
 				var enabled;
-				var unbind = scope.$on('$routeChangeStart', function(ev, currRoute){
+				var unbind = scope.$on('$routeChangeSuccess', function(ev, currRoute){
 					if(enabled){
-						console.log('view enabled');
-						el.removeAttr('mg-view');
-						el.attr('ng-view', '');
+						var view = el.clone().empty();
+						view.removeAttr('mg-view');
+						view.attr('ng-view', '');
+						el.after(view);
+						$animate.leave(el);
+						$compile(view)(scope);
 						unbind();
-						$compile(el)(scope);	
 					}
 					enabled = true;
 				});
