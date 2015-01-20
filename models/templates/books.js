21d15900fd10f9be00acd3f1d1df2f5b8230202b
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-20 13:32:43 +0000

    Added author books

--- models/templates/books.js
+++ models/templates/books.js
@@ -4,15 +4,23 @@ var Pr = require('bluebird'),
 	goodreads = require('../../services/goodreads'),
 	querystring = require('querystring');
 
-// @param type [search, list]
-// search, q is required
-
 module.exports = function(args){
 	return new Pr(function(resolve, reject){
-		goodreads.search(querystring.escape(args.query.q)).then(function(books){
-			resolve({
-				books: books
-			});
-		}, reject);
+		if(args.query.q){
+			goodreads.search(querystring.escape(args.query.q)).then(function(books){
+				resolve({
+					books: books
+				});
+			}, reject);
+		} else if(args.query.author){
+			goodreads.booksByAuthor(querystring.escape(args.query.author)).then(function(books){
+				resolve({
+					books: books
+				});
+			}, reject);
+		} else {
+			resolve();
+		}
+
 	});
 };
--- services/goodreads.js
+++ services/goodreads.js
@@ -6,10 +6,53 @@ var request = require('request-promise'),
 	xml2js = require('xml2js'),
 	parser = Pr.promisify((new xml2js.Parser()).parseString),
 	SEARCH = 'https://www.goodreads.com/search/index.xml?q=%s&key=' + process.env.GOODREADS_KEY,
-	GET = 'https://www.goodreads.com/book/show/%s?key='+ process.env.GOODREADS_KEY,
-	AUTHOR = 'https://www.goodreads.com/author/show/%s.xml?key='+ process.env.GOODREADS_KEY;
+	GET = 'https://www.goodreads.com/book/show/%s?key=' + process.env.GOODREADS_KEY,
+	AUTHOR = 'https://www.goodreads.com/author/show/%s.xml?key=' + process.env.GOODREADS_KEY,
+	BOOKS_BY_AUTHOR = 'https://www.goodreads.com/author/list/%s.xml?key=' + process.env.GOODREADS_KEY;
+
+var _formatBook = function(book){
+	var obj = ['id', 'isbn', 'title', 'isbn13', 'description', 'publisher', 'average_rating', 'num_pages'].reduce(function(obj, current){
+		obj[current] = book[current][0];
+		return obj;
+	}, {});
+	obj.publicationDate = new Date(book.publication_year[0], book.publication_month[0], book.publication_day[0]);
+	obj.image = book.image_url[0].replace(/(\d+)[m,s]\//, '$1l/');
+	obj.authors = book.authors.map(function(item){
+		var author = item.author[0];
+		return ['name', 'image_url', 'average_rating', 'id'].reduce(function(o, current){
+			o[current] = author[current][0];
+			return o;
+		}, {})
+	});
+	return obj;
+};
 
 module.exports = {
+	booksByAuthor: function(id){
+		if(!!id){
+			console.log('Request => ' + util.format(BOOKS_BY_AUTHOR, id));
+			return request(util.format(BOOKS_BY_AUTHOR, id), {
+				rejectUnauthorized: false
+			})
+				.then(function(response){
+					return parser(response);
+				}, function(err){
+					console.log(err);
+				})
+				.then(function(response){
+					return response.GoodreadsResponse.author[0];
+				})
+				.then(function(author){
+					return author.books[0].book.slice(0, 12).map(_formatBook);
+				});
+		} else {
+			return new Pr(function(resolve, reject){
+				reject({
+					error: 'ID must be specified'
+				});
+			});
+		}
+	},
 	author: function(id){
 		if(!!id){
 			console.log('Request => ' + util.format(AUTHOR, id));
@@ -31,9 +74,6 @@ module.exports = {
 					}, {});
 					obj.fans_count = author.fans_count[0]._;
 					obj.image = author.image_url[0];
-					obj.books = author.books[0].book.map(function(book){
-						return book.id[0]._;
-					});
 					return obj;
 				});
 		} else {
@@ -58,22 +98,7 @@ module.exports = {
 				.then(function(response){
 					return response.GoodreadsResponse.book[0];
 				})
-				.then(function(book){
-					var obj = ['id', 'isbn', 'title', 'isbn13', 'description', 'publisher', 'average_rating', 'num_pages'].reduce(function(obj, current){
-						obj[current] = book[current][0];
-						return obj;
-					}, {});
-					obj.publicationDate = new Date(book.publication_year[0], book.publication_month[0], book.publication_day[0]);
-					obj.image = book.image_url[0].replace(/(\d+)[m,s]\//, '$1l/');
-					obj.authors = book.authors.map(function(item){
-						var author = item.author[0];
-						return ['name', 'image_url', 'average_rating', 'id'].reduce(function(o, current){
-							o[current] = author[current][0];
-							return o;
-						}, {})
-					});
-					return obj;
-				});
+				.then(_formatBook);
 		} else {
 			return new Pr(function(resolve, reject){
 				reject({
@@ -111,7 +136,7 @@ module.exports = {
 							title: item.title[0],
 							image: item.image_url[0].replace(/(\d+)[m,s]\//, '$1l/')
 						}
-					})
+					});
 				});
 		} else {
 			return new Pr(function(resolve, reject){
