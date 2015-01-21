9d117e5ff640cf26b87900c20520ae0ce620a87a
Vilmos Ioo <vilmosi@blinkbox.com>
2015-01-21 18:23:20 +0000

    Forgot to minify magma.js

--- grunt/ngAnnotate.js
+++ grunt/ngAnnotate.js
@@ -3,7 +3,8 @@
 module.exports = {
 	dist: {
 		files: {
-			'<%= pck.config.tmp %>/concat/scripts/app.js': ['<%= pck.config.tmp %>/concat/scripts/app.js']
+			'<%= pck.config.tmp %>/concat/scripts/app.js': ['<%= pck.config.tmp %>/concat/scripts/app.js'],
+			'<%= pck.config.tmp %>/concat/scripts/magma.js': ['<%= pck.config.tmp %>/concat/scripts/magma.js']
 		}
 	}
 };
