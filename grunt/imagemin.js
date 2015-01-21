674a8cee0374621ccdabc43bbb6d3f82dcc8f9d7
Vilmos Ioo <cool.villi@gmail.com>
2015-01-21 19:58:30 +0000

    Imagemin to use only images folder

--- grunt/imagemin.js
+++ grunt/imagemin.js
@@ -4,9 +4,9 @@ module.exports = {
 	all: {
 		files: [{
 			expand: true,
-			cwd: '<%= pck.config.app %>',
+			cwd: '<%= pck.config.app %>/images',
 			src: ['**/*.{png,jpg,gif}'],
-			dest: '<%= pck.config.dist %>'
+			dest: '<%= pck.config.dist %>/images'
 		}]
 	}
 };
